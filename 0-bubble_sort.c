94b20acff38ff7b6505b7d1843448dea735555b2
jennycode7 <jennessecosmas7@gmail.com>
2024-04-04 04:09:40 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,8 +23,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				flag = 1;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 
 		if (flag == 0)
