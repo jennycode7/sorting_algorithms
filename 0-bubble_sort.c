822bbdeac6cc71ab3d179f8cb5f2597a06065636
jennycode7 <jennessecosmas7@gmail.com>
2024-04-04 03:48:59 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * buuble_sort - check the code for
+ * array: array of data
+ * size: size of array
+ * Return: Always EXIT_SUCCESS.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+	int flag = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+			}
+			print_array(array, size);
+		}
+
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
